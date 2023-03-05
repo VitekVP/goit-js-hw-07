c4a131763fee2392176b0b09298e49a2cda24f7f
VitekVP <viktor83pochynok@gmail.com>
2023-03-05 18:14:20 +0200

    готовое на проверку

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeItemGallery = ({ preview, original, description }) => {
+	return `<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+		</a>
+	</div>`;
+};
+
+const markupGallery = galleryItems.map(el => makeItemGallery(el)).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+
+const handleClickImgGallery = event => {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const modalWindow = basicLightbox.create(`
+  <img class="gallery__image" src="${event.target.dataset.source}" alt="${event.target.alt}"/>
+`);
+	modalWindow.show();
+
+	document.addEventListener("keydown", handleModalWindowEsc);
+
+	function handleModalWindowEsc(event) {
+		if (event.code === "Escape") {
+			modalWindow.close();
+			document.removeEventListener("keydown", handleModalWindowEsc);
+		}
+	}
+};
+
+galleryEl.addEventListener("click", handleClickImgGallery);
